ff5a87043f1061f75ed0be4a40b31b27135578e3
Cesar Vega <cesavegl@gmail.com>
2023-05-20 22:33:04 -0500

    Despliegue en web de los datos

--- blog-platzi/src/main.rs
+++ blog-platzi/src/main.rs
@@ -5,37 +5,96 @@ pub mod models;
 pub mod schema;
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-use dotenvy::dotenv;
-use std::env;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
 
-use self::models::Post;
+use tera::Tera;
+
+use self::models::{NewPostHandler, Post};
 use self::schema::posts::dsl::*;
-use actix_web::web::Data;
-use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use diesel::r2d2::Pool;
 use diesel::r2d2::{self, ConnectionManager};
 
+#[get("/prueba_tera")]
+async fn tera_test(template_manager: web::Data<tera::Tera>) -> impl Responder {
+    let ctx = tera::Context::new();
+    //ctx.insert("name", "Tera");
+
+    return HttpResponse::Ok()
+        .content_type("text/html")
+        .body(template_manager.render("index.html", &ctx).unwrap());
+}
+
 #[get("/")]
-async fn index(pool: web::Data<DbPool>) -> impl Responder {
+async fn index(pool: web::Data<DbPool>, template_manager: web::Data<tera::Tera>) -> impl Responder {
     let mut conn = pool.get().expect("Problemas al traer la BD");
-    
-    match web::block(move || {posts.load::<Post>(&mut conn)}).await {
+
+    match web::block(move || posts.load::<Post>(&mut conn)).await {
         Ok(data) => {
-            //println!("{:?}", data);
-            HttpResponse::Ok().body(format!("{:?}", data))
-        },
-        Err(err) => {
-            HttpResponse::Ok().body(format!("Error al recibir la data: {}", err))
+            let mut context = tera::Context::new();
+            context.insert("posts", &data.unwrap());
+            let rendered = template_manager
+                .render("index.html", &context)
+                .unwrap_or_else(|err| format!("Error al renderizar: {}", err));
+            HttpResponse::Ok().content_type("text/html").body(rendered)
         }
+        Err(err) => HttpResponse::Ok().body(format!("Error al recibir la data: {}", err)),
     }
 }
 
-#[get("/{name}")]
-async fn hello_world(name: web::Path<String>) -> impl Responder {
-    format!("Hello {}!", &name)
+#[get("/blog/{blog_slug}")]
+async fn get_post(
+    pool: web::Data<DbPool>,
+    template_manager: web::Data<tera::Tera>,
+    blog_slug: web::Path<String>,
+) -> impl Responder {
+    let mut conn = pool.get().expect("Problemas al traer la BD");
+    let url_slug = blog_slug.into_inner();
+
+    match web::block(move || posts.filter(slug.eq(url_slug)).load::<Post>(&mut conn)).await {
+        Ok(data) => {
+            let mut context = tera::Context::new();
+            let data = data.unwrap();
+
+            if data.len() == 0 {
+                return HttpResponse::NotFound().body("No encontrado");
+            }
+
+            let data = &data[0];
+            context.insert("post", data);
+
+            let rendered = template_manager
+                .render("posts.html", &context)
+                .unwrap_or_else(|err| format!("Error al renderizar: {}", err));
+            HttpResponse::Ok().content_type("text/html").body(rendered)
+        }
+        Err(err) => HttpResponse::Ok().body(format!("Error al recibir la data: {}", err)),
+    }
 }
+
+#[post("/new_post")]
+async fn new_post(pool: web::Data<DbPool>, item: web::Json<NewPostHandler>) -> impl Responder {
+    let conn = pool.get().expect("Problemas al traer la BD");
+
+    println!("{:?}", item);
+
+    match web::block(move || Post::create_post(conn, &item)).await {
+        Ok(data) => {
+            //println!("{:?}", data);
+            HttpResponse::Ok().body(format!("{:?}", data))
+        }
+        Err(err) => HttpResponse::Ok().body(format!("Error al recibir la data: {}", err)),
+    }
+}
+
+// #[get("/{name}")]
+// async fn hello_world(name: web::Path<String>) -> impl Responder {
+//     format!("Hello {}!", &name)
+// }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -49,10 +108,15 @@ async fn main() -> std::io::Result<()> {
         .expect("No se pudo construir la Pool");
 
     HttpServer::new(move || {
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
+
         App::new()
             .service(index)
-            .service(hello_world)
-            .app_data(Data::new(pool.clone()))
+            .service(new_post)
+            .service(tera_test)
+            .service(get_post)
+            .app_data(web::Data::new(pool.clone()))
+            .app_data(web::Data::new(tera))
     })
     .bind(("127.0.0.1", 8090))
     .unwrap()
--- blog-platzi/src/models.rs
+++ blog-platzi/src/models.rs
@@ -1,9 +1,22 @@
-#[derive(Queryable)]
-#[derive(Debug)]
+use diesel::{PgConnection, RunQueryDsl, r2d2::{PooledConnection, ConnectionManager}};
+use serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Debug, Deserialize, Serialize)]
+pub struct PostSimplificado {
+    pub title: String,
+    pub body: String,
+}
+
+#[derive(Queryable, Debug, Deserialize, Serialize)]
 pub struct Post {
     pub id: i32,
     pub title: String,
     pub slug: String,
+    pub body: String,
+}
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct NewPostHandler {
+    pub title: String,
     pub body: String,
 }
 
@@ -13,5 +26,27 @@ use super::schema::posts;
 pub struct NewPost<'a> {
     pub title: &'a str,
     pub body: &'a str,
-    pub slug: &'a str,    
+    pub slug: &'a str,
+}
+
+impl Post {
+    pub fn slugify(title: &String) -> String {
+        return title.replace(" ", "-").to_lowercase();
+    }
+
+    pub fn create_post<'a>(
+        mut conn: PooledConnection<ConnectionManager<PgConnection>>,
+        post: &NewPostHandler
+    ) -> Result<Post, diesel::result::Error> {
+        let slug = Post::slugify(&post.title.clone());
+
+        let new_post = NewPost {
+            slug: &slug,
+            title: &post.title,
+            body: &post.body,
+        };
+        diesel::insert_into(posts::table)
+            .values(new_post)
+            .get_result::<Post>(&mut conn)
+    }
 }
